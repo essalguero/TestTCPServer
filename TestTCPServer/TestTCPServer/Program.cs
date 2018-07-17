b8a8f3a7f646fac65e9e15d0be4eae56f46bb9c1
Unknown <essalguero@live.u-tad.com>
2018-07-17 23:03:02 +0200

    Starting

---
+++ TestTCPServer/TestTCPServer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TestTCPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            byte[] localAddress = { 127, 0, 0, 1 };
+            int port = 33;
+            System.Net.IPAddress address = new System.Net.IPAddress(localAddress);
+
+            TCPServer server = new TCPServer(address, port);
+
+        }
+    }
+}
---
+++ TestTCPServer/TestTCPServer/TCPServer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Net.Sockets;
+using System.Net;
+
+namespace TestTCPServer
+{
+    class TCPServer : TcpListener
+    {
+        private Socket listener;
+        private IPEndPoint localEP;
+
+        public TCPServer(IPAddress address, int port) : base(address, port)
+        {
+            Console.WriteLine("TCP Server created");
+
+            localEP = new IPEndPoint(address, port);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        void acceptConnection(IAsyncResult ar)
+        {
+            Socket listener = (Socket)ar.AsyncState;
+            Socket handler = listener.EndAccept(ar);
+        }
+
+        void startListening()
+        {
+            listener = new Socket(localEP.Address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(localEP);
+
+                listener.Listen(20);
+
+                while (true)
+                {
+                    listener.BeginAccept(new AsyncCallback(acceptConnection), listener);
+
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        
+
+
+    }
+}
