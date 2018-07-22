e7d8821ce185ff8b27a553c07e67640f1f0d81ef
Unknown <essalguero@live.u-tad.com>
2018-07-22 17:41:26 +0200

    First draft of communication program

---
+++ TestTCPServer/TestTCPServer/ConnectionInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Net.Sockets;
+using System.Net;
+
+namespace TestTCPServer
+{
+    public class ConnectionInfo
+    {
+        public Socket connectionSocket { get; set; }
+
+
+        // Client  socket.  
+        public Socket workSocket = null;
+        // Size of receive buffer.  
+        public const int BufferSize = 1024;
+        // Receive buffer.  
+        public byte[] buffer = new byte[BufferSize];
+        // Received data string.  
+        public StringBuilder sb = new StringBuilder();
+
+        public ConnectionInfo()
+        {
+
+        }
+
+    }
+}
--- TestTCPServer/TestTCPServer/TCPServer.cs
+++ TestTCPServer/TestTCPServer/TCPServer.cs
@@ -4,6 +4,8 @@ using System.Text;
 
 using System.Net.Sockets;
 using System.Net;
+using System.Threading;
+
 
 namespace TestTCPServer
 {
@@ -12,11 +14,34 @@ namespace TestTCPServer
         private Socket listener;
         private IPEndPoint localEP;
 
+        private ConnectionInfo info = new ConnectionInfo();
+
+        private bool finishComunication = false;
+
+        public static ManualResetEvent receptionEvent = new ManualResetEvent(false);
+
+        public delegate void TcpServerEventDlg(object sender, TcpServerEventDlg e);
+
+
+
+        private AsyncCallback ConnectionReady;
+        private WaitCallback AcceptConnection;
+        private AsyncCallback ReceivedDataReady;
+
+        internal byte[] buffer;
+
+
         public TCPServer(IPAddress address, int port) : base(address, port)
         {
             Console.WriteLine("TCP Server created");
 
-            localEP = new IPEndPoint(address, port);
+            localEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+
+
+            ConnectionReady = new AsyncCallback(ConnectionReady_Handler);
+            //AcceptConnection = new WaitCallback(AcceptConnection_Handler());
+            ReceivedDataReady = new AsyncCallback(ReceivedDataReady_Handler);
+
         }
 
         public override bool Equals(object obj)
@@ -34,15 +59,55 @@ namespace TestTCPServer
             return base.ToString();
         }
 
-        void acceptConnection(IAsyncResult ar)
+        void AcceptConnection_Handler(IAsyncResult ar)
         {
-            Socket listener = (Socket)ar.AsyncState;
-            Socket handler = listener.EndAccept(ar);
+
+
+            //IAsyncResult ar = new IAsyncResult();
+            //Socket listener = (Socket)ar.AsyncState;
+            //Socket handler = listener.EndAccept(ar);
+            Socket senderlistener = ar.AsyncState as Socket;
+            Socket sender = listener.EndAccept(ar);
+
+            ConnectionInfo info = new ConnectionInfo();
+
+            info.connectionSocket = sender;
+            
+
+            //sender.BeginReceive(info.buffer, 0, info.BufferSize, SocketFlags.None, ReceivedDataReady, info);
+
+            
         }
 
-        void startListening()
+        /// <SUMMARY>
+        /// Executes OnReceiveData method from the service provider.
+        /// </SUMMARY>
+        private void ReceivedDataReady_Handler(IAsyncResult ar)
         {
-            listener = new Socket(localEP.Address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            ConnectionInfo info = ar.AsyncState as ConnectionInfo;
+
+            listener.EndReceive(ar);
+            //Im considering the following condition as a signal that the
+            //remote host droped the connection.
+            if (listener.Available != 0)
+            {
+                listener.Close();
+            }
+            else
+            {
+                listener.Close();
+            }
+        }
+
+        private void ConnectionReady_Handler(IAsyncResult ar)
+        {
+
+        }
+
+        public void Start()
+        {
+            listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
             try
             {
@@ -50,10 +115,14 @@ namespace TestTCPServer
 
                 listener.Listen(20);
 
-                while (true)
-                {
-                    listener.BeginAccept(new AsyncCallback(acceptConnection), listener);
+                //ConnectionInfo info = new ConnectionInfo();
 
+                IAsyncResult result = listener.BeginAccept(new AsyncCallback(AcceptConnection), listener);
+
+                while (!finishComunication)
+                {
+                    
+                    //loop to avid the finalization of the server
 
                 }
             }
